c4cdfb16efef96e53dbfc4af7b1b2c65ef44cde6
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-03-25 19:51:49 -0300

    Initial Commit

---
+++ WhatsAoo Chats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsAoo Chats
+//
+//  Created by Felipe Brigagão de Almeida on 25/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
